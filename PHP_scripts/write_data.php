1420e9a72a5d433a4ef0da61401bf2b600b605be
ajouni <ahmadjouni2@gmail.com>
2020-12-28 19:57:42 +0200

    Update write_data.php

--- PHP_scripts/write_data.php
+++ PHP_scripts/write_data.php
@@ -2,9 +2,9 @@
 
     // Prepare variables for database connection
    
-    $dbusername = "dbaycom_ahmad";  // enter database username, I used "arduino" in step 2.2
-    $dbpassword = "Mypass2016";  // enter database password, I used "arduinotest" in step 2.2
-    $server = "www.2dbay.com"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
+    $dbusername = "testuser";  // enter database username, I used "arduino" in step 2.2
+    $dbpassword = "testpass";  // enter database password, I used "arduinotest" in step 2.2
+    $server = "www.test.com"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
 
     // Connect to your database
 
